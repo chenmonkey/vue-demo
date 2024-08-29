98be3ae172e3a5d09ec3c51a94989f25bb20ad34
2446729899 <2446729899@qq.com>
2024-08-29 10:55:50 +0800

    add: 代码提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,34 +3,36 @@
 
   <div class="card">
     <button type="button" @click="openModal">打开model弹窗</button>
-   
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, unref, reactive, computed, provide, h } from 'vue';
- import { VxeUI } from 'vxe-pc-ui'
-import Test from './Test.vue';
+import { defineProps, h } from "vue";
+import { VxeUI } from "vxe-pc-ui";
+import Test from "./Test.vue";
 
-  function  openModal() {
-    const saleInfo = { saleCode: '111', saleId: '222' };
-    VxeUI.modal.open({
-      title: `测试弹窗`,
-      mask: false,
-      lockView: false,
-      escClosable: true,
-      width: 1000,
-      height: 800,
-      showZoom: true,
-      resize: true,
-      slots: {
-        default() {
-          return h(Test, { props: { saleInfo } })
-        }
-      }
-    })
-    
-  }
+defineProps({
+  msg: { type: String, default: "" },
+});
+
+function openModal() {
+  const saleInfo = { saleCode: "111", saleId: "222" };
+  VxeUI.modal.open({
+    title: `测试弹窗`,
+    mask: false,
+    lockView: false,
+    escClosable: true,
+    width: 1000,
+    height: 800,
+    showZoom: true,
+    resize: true,
+    slots: {
+      default() {
+        return h(Test, { props: { saleInfo } });
+      },
+    },
+  });
+}
 </script>
 
 <style scoped>
